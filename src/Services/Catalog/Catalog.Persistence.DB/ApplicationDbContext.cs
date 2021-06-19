d8bbe1fe24b5831d904548500099fecdf0d507e8
Livingstone Cano <lcano@hypernovalabs.com>
2021-06-19 06:37:54 -0600

    Registro inial

---
+++ src/Services/Catalog/Catalog.Persistence.DB/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.Domain;
+using Catalog.Persistence.DB.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Persistence.DB
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInStock> Stocks { get; set; }
+
+
+        //Parar crear la configuraion de los objetos en la base
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Database Schema, se define el esquema a utilizar en la base de datos.
+            builder.HasDefaultSchema("Catalog");
+
+            ModelConfig(builder);
+        }
+
+        private void ModelConfig(ModelBuilder modelBuilder)
+        {
+            new ProductConfiguration(modelBuilder.Entity<Product>());
+            new ProductInStockConfiguration(modelBuilder.Entity<ProductInStock>());
+
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Service.Queries/DTOs/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Service.Queries.DTOs
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        //Relacion a uno
+        public ProductInStockDto Stock { get; set; }
+
+    }
+}
